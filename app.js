9a938db7859657b16d873f5cf600f01b7e7735ef
root <132725436+tingtung46@users.noreply.github.com>
2025-04-15 11:21:50 +0700

    Modify into routes

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+const indexRouter = require("./routes/indexRouter");
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
 
-app.get("/about", (req, res) =>
-  res.sendFile(path.join(__dirname + "/about.html"))
-);
+const PORT = 3000;
 
-app.get("/contact-me", (req, res) =>
-  res.sendFile(path.join(__dirname + "/contact-me.html"))
-);
-
-app.use((req, res) =>
-  res.status(404).sendFile(path.join(__dirname + "/404.html"))
-);
-
-app.listen(process.env.port || 3000);
-
-console.log("Running at port 3000");
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening to port ${PORT}!`);
+});
