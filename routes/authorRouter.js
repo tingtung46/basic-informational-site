0f1f03c4a8baf84f9cacc6221cded6d1d7077c91
root <132725436+tingtung46@users.noreply.github.com>
2025-04-23 11:08:34 +0700

    Modify the author controller

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,9 @@
 const { Router } = require("express");
+const { getAuthorById } = require("../controllers/authorController");
 
 const authorRouter = Router();
 
 authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-});
+authorRouter.get("/:authorId", getAuthorById);
 
 module.exports = authorRouter;
